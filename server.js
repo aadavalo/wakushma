3f357a781e8ba31cf73eda53939b5767511ea0d0
amparo <aadavalo4@gmail.com>
2019-05-28 21:59:20 -0700

    changed about img ref

--- server.js
+++ server.js
@@ -5,18 +5,9 @@ var http = require ('http');
 
 app.set ("port", process.env.PORT || 3000)
 
-// Create an instance of the http server to handle HTTP requests
-//let app = http.createServer((req, res) => {
-    // Set a response type of plain text for the response
-    //res.writeHead(200, {'Content-Type': 'text/plain'});
-
     /* serve the static files */
 	  app.use(express.static('static-files/'));
-  	//app.use(express.static('images/'));
 
-//});
-
-// app.get('/', (req, res) => res.send('index.html'))
 
 // Start the server on port 3000
 // app.listen(3000, '127.0.0.1');
